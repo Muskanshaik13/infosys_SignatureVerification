e6e4d55f18a8627521193df6fffe2f5937987ea1
Muskanshaik13 <muskanshaik1308@gmail.com>
2024-11-07 22:53:36 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-# auth_app/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('upload/', views.upload, name='upload'),
-]
